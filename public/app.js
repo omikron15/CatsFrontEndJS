1107d007af1de8a72055c59ca158095ce4ebad27
omikron15 <omikron15@hotmail.co.uk>
2018-06-11 19:28:08 +0100

    refactors index.html to remove hard coded cats and add them to app.js

--- public/app.js
+++ public/app.js
@@ -1,5 +1,7 @@
 var app = function() {
-
+addCat("Boba", "Sock fluff", "http://66.media.tumblr.com/d1f01bbe0150fda0c40d2151c5eaeac8/tumblr_odlqqskjj61v9cejwo1_400.jpg");
+addCat("Barnaby", "Tuna", "https://68.media.tumblr.com/88d0fcf2b84a7b098dda839130597569/tumblr_okuo4teiql1uhevdso1_1280.jpg");
+addCat("Max", "Whiskas Temptations", "http://66.media.tumblr.com/7c5784ea89369c780e782bf10c60315a/tumblr_npb0hlYwhV1u63jaco1_1280.jpg");
 addCat("Cat1", "Food 1", "https://i.pinimg.com/originals/f2/d1/76/f2d17682de7dafc59cd11695478de2bd.jpg");
 
 };
